7feb068b96644e1ac65f3c64ebb39f94100f79c7
Brad Ross <brad.ross.35@gmail.com>
2015-01-14 13:54:21 -0800

    added driveTrain initialization in driveTrainContoller

--- src/org/usfirst/frc/team4990/robot/DriveTrainController.java
+++ src/org/usfirst/frc/team4990/robot/DriveTrainController.java
@@ -2,9 +2,11 @@ package org.usfirst.frc.team4990.robot;
 
 public class DriveTrainController {
 	F310Gamepad gamepad;
+	DriveTrain driveTrain;
 	
 	public DriveTrainController(F310Gamepad gamepad, DriveTrain driveTrain) {
 		this.gamepad = gamepad;
+		this.driveTrain = driveTrain;
 	}
 	
 	

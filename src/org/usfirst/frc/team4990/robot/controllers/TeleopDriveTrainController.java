e9247093cc7327c1658b44732a408c7e029d7a8d
wileyyugioh <waterballthatsonfire@gmail.com>
2015-02-02 16:09:26 -0800

    Update TeleopDriveTrainController.java

--- src/org/usfirst/frc/team4990/robot/controllers/TeleopDriveTrainController.java
+++ src/org/usfirst/frc/team4990/robot/controllers/TeleopDriveTrainController.java
@@ -76,4 +76,8 @@ public class TeleopDriveTrainController {
 		 */
 		this.driveTrain.setSpeed(turningSpeed, -turningSpeed);
 	}
+	public boolean isTurningReversed(){
+	//basic get method for reversed turning
+		return reverseTurningFlipped;
+	}
 }

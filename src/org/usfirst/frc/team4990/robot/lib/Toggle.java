c1e5f52915787052ad4c72614628bcd4fa5e96c2
gryphonrobotics <gryphonrobotics@csus.org>
2015-02-12 17:41:38 -0800

    fixed toggle implementation

--- src/org/usfirst/frc/team4990/robot/lib/Toggle.java
+++ src/org/usfirst/frc/team4990/robot/lib/Toggle.java
@@ -6,15 +6,16 @@ public class Toggle {
 	private boolean toggleState;
 	private Date timeLastToggled;
 	
-	private final int timeUntilNextToggle;
+	private final int millisUntilNextToggle;
 	
-	public Toggle(int timeUntilNextToggle) {
-		this.timeUntilNextToggle = timeUntilNextToggle;
+	public Toggle(int millisUntilNextToggle) {
+		this.millisUntilNextToggle = millisUntilNextToggle;
+		this.timeLastToggled = new Date(java.lang.System.currentTimeMillis() - this.millisUntilNextToggle);
 	}
 	
 	public void toggle() {
 		long timeSinceLastToggle = (new Date()).getTime() - this.timeLastToggled.getTime();
-		if (timeSinceLastToggle >= timeUntilNextToggle) {
+		if (timeSinceLastToggle >= this.millisUntilNextToggle) {
 			this.toggleState = !this.toggleState;
 			this.timeLastToggled = new Date();
 		}

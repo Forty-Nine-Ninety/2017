3af41f13ab945a5832cd629bb127e65b96fae038
Brad Ross <brad.ross.35@gmail.com>
2015-02-13 18:24:08 -0800

    added solenoid code

--- src/org/usfirst/frc/team4990/robot/subsystems/Fork.java
+++ src/org/usfirst/frc/team4990/robot/subsystems/Fork.java
@@ -1,12 +1,13 @@
 package org.usfirst.frc.team4990.robot.subsystems;
 
 public class Fork {
+	private Solenoid solenoid;
 	
-	public Fork() {
-		
+	public Fork(int pcmChannelNum) {
+		this.solenoid = new Solenoid(pcmChannelNum);
 	}
 	
 	public void setForkState(boolean isOpen) {
-		
+		this.solenoid.set(isOpen);
 	}
 }

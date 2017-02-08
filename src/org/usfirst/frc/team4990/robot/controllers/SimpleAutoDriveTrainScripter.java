9bc148334134915b9439ec56b0dd0d9c9b7dfd3b
gryphonrobotics <gryphonrobotics@csus.org>
2017-02-07 17:21:09 -0800

    Start framework 4 auto scripting & finish? forward

---
+++ src/org/usfirst/frc/team4990/robot/controllers/SimpleAutoDriveTrainScripter.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team4990.robot.controllers;
+
+import org.usfirst.frc.team4990.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team4990.robot.controllers.AutoDriveTrainScripter;
+
+public class SimpleAutoDriveTrainScripter extends AutoDriveTrainScripter {
+	
+	public void init()
+	{
+		
+	}
+	
+	public SimpleAutoDriveTrainScripter(DriveTrain dtrain) {
+		super(dtrain);
+		init();
+	}
+
+}
